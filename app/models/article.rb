66e2841025543b175d0d55b7fb165e5059ed034a
Stefan Dasbach <StefanDasbach@gmail.com>
2015-02-22 18:02:08 -0500

    Fix typo in article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ActiveRecord::Base
   belongs_to :category
 
-  validates_presnce_of :title
+  validates_presence_of :title
   validates_presence_of :content
 
   scope :alphabetical, order('title')
